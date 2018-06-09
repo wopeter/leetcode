a54ae4011aea4c1a4013c751bf356296d4ae7c33
wopeter <pengtemail@163.com>
2018-06-10 00:45:15 +0800

    No. 46

---
+++ 46_Permutations.py
@@ -0,0 +1,39 @@
+"""
+No.46 Permutations
+
+Description:
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+"""
+class Solution:
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.def(nums, [], res)
+        return res
+        
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+        for i in range(len(nums)):
+            self.dfs(nums[:i] + nums[i+1:], path+[nums[i]], res)
+            
+    def permute2(self, nums):
+        import itertools
+        return list(itertools.permutations(nums))
+    
