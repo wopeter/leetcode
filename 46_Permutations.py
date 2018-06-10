d4e394af79a8b4a619e0b7845018b636b1d7819c
wopeter <pengtemail@163.com>
2018-06-10 10:26:44 +0800

    add another solution

--- 46_Permutations.py
+++ 46_Permutations.py
@@ -18,13 +18,14 @@ Output:
 ]
 """
 class Solution:
+    #solution 1
     def permute(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
         res = []
-        self.def(nums, [], res)
+        self.dfs(nums, [], res)
         return res
         
     def dfs(self, nums, path, res):
@@ -32,8 +33,24 @@ class Solution:
             res.append(path)
         for i in range(len(nums)):
             self.dfs(nums[:i] + nums[i+1:], path+[nums[i]], res)
-            
+    
+    #solution 2
     def permute2(self, nums):
         import itertools
         return list(itertools.permutations(nums))
+        
+    #solution 3   
+    def permute3(self, nums):
+        self.res = []
+        self.dfs(nums, 0, len(nums)-1)
+        return self.res
+    def dfs(self, nums, k, m):
+        if k == m:
+            self.res.append(nums[:])
+        else:
+            for i in range(k, m+1):
+                nums[i], nums[k] = nums[k], nums[i]
+                self.dfs(nums, k+1, m)
+                nums[i], nums[k] = nums[k], nums[i]
+           
     
